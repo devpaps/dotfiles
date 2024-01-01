6f5c1441394d71d17f5e65a9e6bd1687fd65ca33
Lars Jönsson <crymeat@gmail.com>
2024-01-01 16:50:16 +0100

    add more configs and optimizations

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -8,5 +8,28 @@ vim.api.nvim_create_augroup("LspNodeModules", { clear = true })
 vim.api.nvim_create_autocmd("BufRead", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)", group = 'LspNodeModules' })
 vim.api.nvim_create_autocmd("BufNewFile", { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)", group = 'LspNodeModules' })
 
+vim.cmd("command! EslintFixAll lua RunEslintFixAll()")
+
+function RunEslintFixAll()
+    local job_id = vim.fn.jobstart({"eslint", "--fix", vim.fn.expand("%")}, {
+        on_exit = function(j, exit_code, event)
+            if exit_code == 0 then
+                vim.cmd("e")
+            else
+                print("ESLint failed. Exit code:", exit_code)
+            end
+        end,
+        detach = true
+    })
+end
+
+vim.api.nvim_create_augroup("Eslint", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.vue",
+    command = "EslintFixAll",
+    group = "Eslint"
+})
+
+
 -- vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
 -- vim.api.nvim_create_autocmd("BufWritePost", { command = "lua vim.lsp.buf.format()", group = 'FormatOnSave' })
--- nvim/.config/nvim/lua/lsp/run.lua
+++ nvim/.config/nvim/lua/lsp/run.lua
@@ -1,24 +1,33 @@
 -- Setup installer & lsp configs
-local typescript_ok, typescript = pcall(require, 'typescript')
-local mason_ok, mason = pcall(require, 'mason')
-local mason_lsp_ok, mason_lsp = pcall(require, 'mason-lspconfig')
+local mason_ok, mason = pcall(require, "mason")
+local mason_lsp_ok, mason_lsp = pcall(require, "mason-lspconfig")
 
 if not mason_ok or not mason_lsp_ok then
   return
 end
 
-mason.setup {
+mason.setup({
   ui = {
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = M42.ui.float.border or "rounded",
-  }
-}
+  },
+})
 
-mason_lsp.setup {
+mason_lsp.setup({
   -- A list of servers to automatically install if they're not already installed
-  ensure_installed = { "bashls", "cssls", "eslint", "graphql", "html", "jsonls", "lua_ls", "tailwindcss", "tsserver",
-    "vuels", "volar", "prismals" },
-
+  ensure_installed = {
+    "bashls",
+    "cssls",
+    "eslint",
+    "graphql",
+    "html",
+    "jsonls",
+    "lua_ls",
+    "prismals",
+    "tailwindcss",
+    "tsserver",
+    "rust_analyzer"
+  },
   -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
   -- This setting has no relation with the `ensure_installed` setting.
   -- Can either be:
@@ -27,90 +36,184 @@ mason_lsp.setup {
   --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
   --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
   automatic_installation = true,
-}
+
+  -- Correctly setup lspconfig for Rust 🚀
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Ensure mason installs the server
+        rust_analyzer = {
+          keys = {
+            { "K",          "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+            { "<leader>cR", "<cmd>RustCodeAction<cr>",   desc = "Code Action (Rust)" },
+            { "<leader>dr", "<cmd>RustDebuggables<cr>",  desc = "Run Debuggables (Rust)" },
+          },
+          settings = {
+            ["rust-analyzer"] = {
+              diagnostics = {
+               enable = true,
+              },
+              cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                runBuildScripts = true,
+              },
+              -- Add clippy lints for Rust.
+              check = {
+                allFeatures = true,
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+              },
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+        },
+        taplo = {
+          keys = {
+            {
+              "K",
+              function()
+                if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+                  require("crates").show_popup()
+                else
+                  vim.lsp.buf.hover()
+                end
+              end,
+              desc = "Show Crate Documentation",
+            },
+          },
+        },
+      },
+      setup = {
+        rust_analyzer = function(_, opts)
+          local rust_tools_opts = require("lazyvim.util").opts("rust-tools.nvim")
+          require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
+          return true
+        end,
+      },
+    },
+  },
+
+})
 
 local lspconfig = require("lspconfig")
 
 local handlers = {
-  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = M42.ui.float.border }),
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    silent = true,
+    border = M42.ui.float.border,
+  }),
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = M42.ui.float.border }),
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { virtual_text = M42.lsp.virtual_text }
+  ),
 }
 
 local function on_attach(client, bufnr)
   -- set up buffer keymaps, etc.
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-if cmp_nvim_lsp_ok then
-  capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-end
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- Order matters
-
--- It enables tsserver automatically so no need to call lspconfig.tsserver.setup
-if typescript_ok then
-  lspconfig.tsserver.setup {
-    capabilities = require('lsp.servers.tsserver').capabilities,
-    handlers = handlers,
-    settings = require('lsp.servers.tsserver').settings,
-    on_attach = require('lsp.servers.tsserver').on_attach,
-  }
-end
-
-lspconfig.cssls.setup {
-  capabilities = capabilities,
-  handlers = handlers,
-  on_attach = require('lsp.servers.cssls').on_attach,
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
 }
 
-lspconfig.tailwindcss.setup {
-  capabilities = require('lsp.servers.tsserver').capabilities,
-  filetypes = require('lsp.servers.tailwindcss').filetypes,
-  handlers = handlers,
-  init_options = require('lsp.servers.tailwindcss').init_options,
-  on_attach = require('lsp.servers.tailwindcss').on_attach,
-  settings = require('lsp.servers.tailwindcss').settings,
+require("mason-lspconfig").setup_handlers {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      handlers = handlers,
+    }
+  end,
+
+  ["tsserver"] = function()
+    -- Skip since we use typescript-tools.nvim
+  end,
+
+  ["tailwindcss"] = function()
+    lspconfig.tailwindcss.setup({
+      capabilities = require("lsp.servers.tailwindcss").capabilities,
+      filetypes = require("lsp.servers.tailwindcss").filetypes,
+      handlers = handlers,
+      init_options = require("lsp.servers.tailwindcss").init_options,
+      on_attach = require("lsp.servers.tailwindcss").on_attach,
+      settings = require("lsp.servers.tailwindcss").settings,
+    })
+  end,
+
+  -- ["rust_analyzer"] = function()
+  --   lspconfig.rust_analyzer.setup({
+  --     on_attach = on_attach,
+  --     capabilities = capabilities,
+  --     handlers = handlers,
+  --     settings = {
+  --       ["rust-analyzer"] = {
+  --         checkOnSave = {
+  --           command = "clippy",
+  --         },
+  --       },
+  --     },
+  --   })
+  -- end,
+
+  ["cssls"] = function()
+    lspconfig.cssls.setup({
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = require("lsp.servers.cssls").on_attach,
+      settings = require("lsp.servers.cssls").settings,
+    })
+  end,
+
+  ["eslint"] = function()
+    lspconfig.eslint.setup({
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = require("lsp.servers.eslint").on_attach,
+      settings = require("lsp.servers.eslint").settings,
+    })
+  end,
+
+  ["jsonls"] = function()
+    lspconfig.jsonls.setup({
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = on_attach,
+      settings = require("lsp.servers.jsonls").settings,
+    })
+  end,
+
+  ["lua_ls"] = function()
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      handlers = handlers,
+      on_attach = on_attach,
+      settings = require("lsp.servers.lua_ls").settings,
+    })
+  end,
+
+  ["vuels"] = function()
+    lspconfig.vuels.setup({
+      filetypes = require("lsp.servers.vuels").filetypes,
+      handlers = handlers,
+      init_options = require("lsp.servers.vuels").init_options,
+      on_attach = require("lsp.servers.vuels").on_attach,
+      settings = require("lsp.servers.vuels").settings,
+    })
+  end
 }
-
-lspconfig.eslint.setup {
-  capabilities = capabilities,
-  handlers = handlers,
-  on_attach = require('lsp.servers.eslint').on_attach,
-  settings = require('lsp.servers.eslint').settings,
-}
-
-lspconfig.jsonls.setup {
-  capabilities = capabilities,
-  handlers = handlers,
-  on_attach = on_attach,
-  settings = require('lsp.servers.jsonls').settings,
-}
-
-lspconfig.omnisharp.setup {
-  handlers = handlers,
-  on_attach = on_attach,
-  settings = require('lsp.servers.omnisharp').settings,
-}
-
-lspconfig.lua_ls.setup {
-  handlers = handlers,
-  on_attach = on_attach,
-  settings = require('lsp.servers.lua_ls').settings,
-}
-
-lspconfig.vuels.setup {
-  filetypes = require('lsp.servers.vuels').filetypes,
-  handlers = handlers,
-  init_options = require('lsp.servers.vuels').init_options,
-  on_attach = on_attach,
-}
-
-
-for _, server in ipairs { "bashls", "cssls", "graphql", "html", "volar", "prismals" } do
-  lspconfig[server].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    handlers = handlers,
-  }
-end
