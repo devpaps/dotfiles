91cb53849829ed01fbd1fe6462b70723d079fbb8
Lars Jönsson <crymeat@gmail.com>
2024-01-06 12:32:08 +0100

    fix(modules): Remove redundant modules thats not being used

--- waybar/.config/waybar/modules/weather.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-text="$(curl -s "https://wttr.in/?format=1" | sed 's/ +//g')"
-tooltip="$(curl -s "https://wttr.in/?0QT" |
-    sed 's/\\/\\\\/g' |
-    sed ':a;N;$!ba;s/\n/\\n/g' |
-    sed 's/"/\\"/g')"
-echo "{\"text\": \"${text}\", \"tooltip\": \"<tt>${tooltip}</tt>\", \"class\": \"weather\"}"
