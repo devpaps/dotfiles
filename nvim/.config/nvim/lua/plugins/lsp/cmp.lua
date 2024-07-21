d39f0e4cd485cd2285ed6bfbb7943341961f3875
Lars Jönsson <crymeat@gmail.com>
2024-07-21 18:23:20 +0200

    feat(cmp): Adding cmp-nvim-lsp to completion

--- nvim/.config/nvim/lua/plugins/lsp/cmp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/cmp.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = {
 			"hrsh7th/cmp-buffer", -- source for text in buffer
 			"hrsh7th/cmp-path", -- source for file system paths
+			"hrsh7th/cmp-nvim-lsp", -- LSP source
 			"L3MON4D3/LuaSnip", -- snippet engine
 			"saadparwaiz1/cmp_luasnip", -- for autocompletion
 			"rafamadriz/friendly-snippets", -- useful snippets
@@ -12,9 +13,7 @@ return {
 		},
 		config = function()
 			local cmp = require("cmp")
-
 			local luasnip = require("luasnip")
-
 			local lspkind = require("lspkind")
 
 			-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
