51296b560c4f83d45c0623805a52087fe8d14573
Lars Jönsson <crymeat@gmail.com>
2025-01-25 20:15:02 +0100

    feat(waybar): add Spotify module and update styles
    - Added custom Spotify module to waybar configuration.
    - Included a new script `spotify-status.sh` to fetch Spotify status.
    - Updated waybar style to support Spotify module.
    - Removed `get_weather.sh` script as i don't need it.
    - Modified clock format in waybar configuration.

---
+++ waybar/.config/waybar/spotify-status.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+status="$(playerctl -p spotify status 2>/dev/null)"  
+# e.g. "Playing", "Paused", or "Stopped"
+
+if [ "$status" = "Playing" ]; then
+  artist_title="$(playerctl -p spotify metadata --format '  {{ artist }} - {{ title }}' 2>/dev/null)"
+  echo "{\"text\": \"$artist_title\", \"class\": \"playing\"}"
+elif [ "$status" = "Paused" ]; then
+  artist_title="$(playerctl -p spotify metadata --format '  {{ artist }} - {{ title }}' 2>/dev/null)"
+  echo "{\"text\": \"$artist_title\", \"class\": \"paused\"}"
+else
+  # Either "Stopped" or Spotify not running
+  echo "{\"text\": \"No music for you my friend\", \"class\": \"stopped\"}"
+fi
