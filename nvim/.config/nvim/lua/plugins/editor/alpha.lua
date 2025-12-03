cf91e53b3376bc66a069face932f755cf30641de
Lars Jönsson <crymeat@gmail.com>
2025-12-03 08:43:30 +0100

    refactor(alpha): streamline dashboard buttons

--- nvim/.config/nvim/lua/plugins/editor/alpha.lua
+++ nvim/.config/nvim/lua/plugins/editor/alpha.lua
@@ -20,11 +20,11 @@ return {
 			dashboard.section.header.val = vim.split(logo, "\n")
     -- stylua: ignore
     dashboard.section.buttons.val = {
-      dashboard.button("p", " " .. " Projects",	      "<cmd> Telescope projects <CR>"),
-      dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
-      dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
+      dashboard.button("p", " " .. " Projects",	      "<cmd> Telescope projects <cr>"),
+      -- dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+      -- dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
       dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
-      dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
+      -- dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
       dashboard.button("c", " " .. " Config",          "<cmd> e $MYVIMRC<CR>"),
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
