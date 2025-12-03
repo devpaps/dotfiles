7c59fcdd8b1c6112823b66e88b3b61f41764a233
Lars Jönsson <crymeat@gmail.com>
2025-12-03 08:44:12 +0100

    feat(fzf): add toggle ignore/hidden actions
    Added alt-i and alt-h keybindings to toggle ignore and hidden files in
    fzf-lua grep and files pickers. Updated rg_opts for better exclusions.

--- nvim/.config/nvim/lua/plugins/editor/fzf.lua
+++ nvim/.config/nvim/lua/plugins/editor/fzf.lua
@@ -4,9 +4,30 @@ return {
 	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		-- calling `setup` is optional for customization
+		local fzf = require("fzf-lua")
+
+		local grep_actions = vim.tbl_extend("force", fzf.defaults.grep.actions, {
+			["alt-i"] = { fzf.actions.toggle_ignore },
+			["alt-h"] = { fzf.actions.toggle_hidden },
+		})
+
+		local files_actions = vim.tbl_extend("force", fzf.defaults.actions.files, {
+			["alt-i"] = { fzf.actions.toggle_ignore },
+			["alt-h"] = { fzf.actions.toggle_hidden },
+		})
+
 		require("fzf-lua").setup({
+			grep = {
+				actions = grep_actions,
+				-- Set base rg_opts with your exclusions (applies always); toggles add/remove --no-ignore and --hidden
+				rg_opts = "--column --line-number --no-heading --color=always --smart-case --glob '!.git/**' --glob '!**/node_modules/**' --glob '!**/vendor/**' --glob '!package-lock.json' --glob '!yarn.lock' --glob '!lazy-lock.json'",
+			},
+			live_grep = {
+				actions = grep_actions,
+				-- rg_opts = "--column --line-number --no-heading --color=always --smart-case --glob '!.git/**' --glob '!**/node_modules/**' --glob '!**/vendor/**' --glob '!package-lock.json' --glob '!yarn.lock' --glob '!lazy-lock.json'",
+			},
 			files = {
+				actions = files_actions,
 				file_ignore_patterns = {
 					"vendor",
 					"node_modules",
