4bc7eef34c0470a3b3578e63b795b877afbc91e0
Lars Jönsson <crymeat@gmail.com>
2026-02-19 16:08:06 +0100

    refactor(nvim): remove kickstart-nvim custom config
    It's not being used

--- kickstart-nvim/.config/kickstart-nvim/lua/plugins/code/comment.lua
+++
@@ -1,5 +0,0 @@
-return {
-	-- "gc" to comment visual regions/lines
-	{ "numToStr/Comment.nvim", opts = {}, event = "VeryLazy" },
-	{ "JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy" },
-}
