e15a0d7f85b9bac211fc2311479f1f38f87bc28c
Lars Jönsson <crymeat@gmail.com>
2026-08-10 07:53:52 +0200

    fix: testing out fixing a bug

--- sway/.config/sway/lockman.sh
+++ sway/.config/sway/lockman.sh
@@ -4,6 +4,14 @@
 #     timeout 10 'swaymsg "output * dpms off"' \
 #     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock -f -c 550000
+# swaylock -f -c 550000
 # Kills last background task so idle timer doesn't keep running
 # kill %%
+
+# Prevent multiple instances
+pgrep -x hyprlock && exit 0
+
+# Run hyprlock in the background so it doesn't block swayidle
+hyprlock &
+# Give it a moment to render before allowing the system to sleep
+sleep 0.5
